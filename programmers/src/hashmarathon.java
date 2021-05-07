95e5c87c61e29a4c3d6f36680c9a609caf218566
GraceNam <zeidgame@gmail.com>
2021-05-07 13:46:08 +0900

    docs : 완주하지 못한 선수
    + 프로그래머스 Lv1
      + 해시

---
+++ programmers/src/hashmarathon.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class hashmarathon {
+
+    // non hash
+    public String solution1(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        int i;
+
+        for(i = 0; i < completion.length; i++) {
+            if(!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+
+        return participant[i];
+    }
+
+    // hash
+    public String solution2(String[] participant, String[] completion) {
+        String answer = "";
+
+        HashMap<String, Integer> hMap = new HashMap<>();
+
+        for(String player : participant) {
+            hMap.put(player, hMap.getOrDefault(player, 0) + 1);
+        }
+
+        for(String player : completion) {
+            hMap.put(player, hMap.get(player) - 1);
+        }
+
+        for(String key : hMap.keySet()) {
+            if(hMap.get(key) != 0) {
+                answer = key;
+            }
+        }
+
+        return answer;
+    }
+
+}
