45f7dfd9fe997095db399c660d101d11d608cf1c
GraceNam <zeidgame@gmail.com>
2021-06-27 21:10:25 +0900

    docs : 더 맵게
    <description>
    + 프로그래머스 Lv2
      + 힙
      + 우선순위 큐

---
+++ programmers/heapmorehot.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class heapmorehot {
+  
+  public int solution1(int[] scoville, int k) {
+    int answer = 0;
+    
+    PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+    for(int aScoville : scoville) {
+      heap.offer(aScoville);
+    }
+
+    while(heap.peek() <= k) {
+      if(heap.size() == 1) {
+        return -1;
+      }
+
+      int a = heap.poll();
+      int b = heap.poll();
+
+      int result = a + (b * 2);
+
+      heap.offer(result);
+      answer++;
+    }
+
+    return answer;
+  }
+}
